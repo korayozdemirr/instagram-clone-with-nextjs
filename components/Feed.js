75c29798c763c3c0cfd6f9bb543928e270aa9848
korayozdemirr <koray.ozdemir11@gmail.com>
2021-12-04 23:50:10 +0300

    Building the Input Box Section of the Post bitti

--- components/Feed.js
+++ components/Feed.js
@@ -1,11 +1,12 @@
 import Stories from "./Stories";
-
+import Posts from "./Posts"
 export default function Feed() {
     return (
-        <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
+        <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
             <section className="col-span-2">
                 {/* stories */}
                 <Stories />
+                <Posts />
                 {/* Posts */}
             </section>
             <section>
---
+++ components/Posts.js
@@ -0,0 +1,34 @@
+import Post from "./Post"
+
+const posts =[
+    {
+        id: "123",
+        username:"korayozdemir",
+        userImg:"https://avatars.githubusercontent.com/u/33378626?v=4",
+        img:"https://avatars.githubusercontent.com/u/33378626?v=4",
+        caption:"hello world"
+    },
+    {
+        id: "123",
+        username:"korayozdemir",
+        userImg:"https://avatars.githubusercontent.com/u/33378626?v=4",
+        img:"https://avatars.githubusercontent.com/u/33378626?v=4",
+        caption:"hello world"
+    }
+]
+
+export default function Posts() {
+    return (
+        <div>
+            {posts.map((post)=>(
+                <Post key={post.id} 
+                id={post.id}
+                username={post.username}
+                userImg={post.userImg}
+                img={post.img}
+                caption={post.caption}
+                />
+            ))}
+        </div>
+    )
+}
